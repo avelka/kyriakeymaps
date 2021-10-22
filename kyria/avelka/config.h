810f2dc368338a841abd661d1ee80034d0b70c29
antoine <antoine@avelka.com>
2021-10-22 19:38:12 +0200

    up thumb row

--- kyria/avelka/config.h
+++ kyria/avelka/config.h
@@ -26,4 +26,8 @@
 
 #define TAPPING_TERM 300
 
-#define PERMISSIVE_HOLD
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
--- kyria/avelka/keymap.c
+++ kyria/avelka/keymap.c
@@ -91,38 +91,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      | Enter|      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
+
     [_COLEMAK_DH] = LAYOUT(
      KC_ESC  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                      KC_J  , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
      KC_TAB  , CTR_A  , ALT_R  , SFT_S  , GIU_T  , KC_G   ,                                      KC_M  , GUI_N  , SFT_E  , ALT_I  , CTR_O  , KC_QUOT,
      NAV     , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, KC_ENT , FKEYS , KC_RBRC, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT ,
-                                 ADJUST , NUM    , KC_SPC , KC_SPC,  CODE   , KC_ENT, KC_RALT, KC_BSPC, SYM    , KC_LGUI
+                                 ADJUST , NUM    , KC_SPC, KC_SPC  , CODE  , KC_RSFT, KC_RALT , KC_BSPC, SYM    , KC_LGUI
     ),
 
 		[_COLEMAK_DH_OSX] = LAYOUT(
      KC_ESC  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                    KC_J  , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
      KC_TAB  , GUI_A  , ALT_R  , SFT_S  , CTR_T  , KC_G   ,                                    KC_M  , CTR_N  , SFT_E  , ALT_I  , GUI_O  , KC_QUOT,
      NAV     , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, KC_ENT , FKEYS , KC_RBRC, KC_K  , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT ,
-                                 ADJUST , NUM    , KC_SPC , KC_SPC , CODE   , KC_ENT, KC_RALT, SYM   , FKEYS  , KC_LGUI
+                                  ADJUST , NUM    , KC_SPC, KC_SPC  , CODE  , KC_RSFT, KC_RALT , KC_BSPC, SYM    , KC_LGUI
     ),
 
     [_NAV] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
       _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 
     [_NUM] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_P7, KC_P8, KC_P9, _______, KC_EQL , KC_BSPC,
-      _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______,                                     KC_P4, KC_P5, KC_P6, _______, KC_PLUS,  KC_DEL,
-      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, KC_P1, KC_P2, KC_P3, _______, KC_SLSH,  KC_ENT,
-                                 _______, _______, _______, _______, _______, KC_NLCK, KC_COMM, KC_P0, KC_DOT,  _______
+      _______, _______, _______, _______, _______, _______,                                     _______, KC_P7 , KC_P8, KC_P9, KC_EQL , KC_BSPC,
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     _______, KC_P4 , KC_P5, KC_P6, KC_PLUS,  KC_DEL,
+      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, _______, KC_P1 , KC_P2, KC_P3, KC_SLSH,  KC_ENT,
+                                 _______, _______, _______, _______, _______, KC_NLCK, KC_COMM, KC_P0  , KC_DOT,  _______
     ),
     
     [_FUNCTION] = LAYOUT(
       _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, _______,
+      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RGUI, KC_RSFT, KC_LALT, KC_RCTL, _______,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
