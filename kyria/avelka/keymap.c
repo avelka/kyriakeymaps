fe037fead9ada7e824f0d7cfee185437b58090c3
antoine <antoine@avelka.com>
2021-10-23 16:17:52 +0200

    num

--- kyria/avelka/keymap.c
+++ kyria/avelka/keymap.c
@@ -35,10 +35,10 @@ enum layers {
     _COLEMAK_DH,
     _COLEMAK_DH_OSX,
     _NAV,
-    _SYM,
     _FUNCTION,
     _ADJUST,
-    _NUM,
+    _NUMR,
+    _NUML,
     _GAMING,
     _CODE
 };
@@ -49,17 +49,18 @@ enum layers {
 #define CMK_OSX  DF(_COLEMAK_DH_OSX)
 #define GAMING   DF(_GAMING)
 
-#define SYM      MO(_SYM)
+
 #define NAV      MO(_NAV)
 #define CODE     MO(_CODE)
-#define NUM      MO(_NUM)
+#define NUM_R    MO(_NUMR)
+#define NUM_L    MO(_NUML)
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
 
-#define SYM_SPC  LT(_SYM, KC_SPC)
-#define NAV_SPC  LT(_NAV, KC_SPC)
+#define NAV_ENT  LT(_NAV, KC_ENT)
 #define COD_SPC  LT(_CODE, KC_SPC)
-#define NUM_SPC  LT(_NUM, KC_SPC)
+#define NBL_SPC  LT(_NUML, KC_SPC)
+#define NBR_SPC  LT(_NUMR, KC_SPC)
 #define FUN_SPC  LT(_FUNCTION, KC_SPC)
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
@@ -72,6 +73,10 @@ enum layers {
 
 #define KC_MOR  LSFT(KC_COMM)
 #define KC_LSS  LSFT(KC_DOT)
+#define KC_COLN  LSFT(KC_SCLN)
+#define KC_SQOT  LSFT(KC_QUOT)
+
+
 
 
 
@@ -101,54 +106,52 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
      KC_ESC  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
      KC_TAB  , CTR_A  , ALT_R  , SFT_S  , GIU_T  , KC_G   ,                                     KC_M   , GUI_N  , SFT_E  , ALT_I  , CTR_O  , KC_QUOT,
-     NAV     , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, SH_TG  , SH_TG  , KC_RBRC, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT,
-                                 ADJUST , KC_LSFT, COD_SPC, NUM_SPC, KC_ENT, KC_BSPC , FUN_SPC, SYM_SPC, KC_RALT, KC_LGUI
+     NAV_ENT , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, SH_TG  , SH_TG  , KC_RBRC, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT,
+                                 ADJUST , KC_LSFT, COD_SPC, NBR_SPC , KC_ENT, KC_DEL , FUN_SPC, NBL_SPC, KC_RALT, KC_LGUI
     ),
 
 		[_COLEMAK_DH_OSX] = LAYOUT(
      KC_ESC  , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSPC,
      KC_TAB  , GUI_A  , ALT_R  , SFT_S  , CTR_T  , KC_G   ,                                     KC_M   , CTR_N  , SFT_E  , ALT_I  , GUI_O  , KC_QUOT,
-     NAV     , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, SH_TG  , SH_TG  , KC_RBRC, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT ,
-                                 ADJUST , KC_LSFT, COD_SPC, NUM_SPC, KC_ENT, KC_BSPC , FUN_SPC, SYM_SPC, KC_RALT, KC_LGUI
+     NAV_ENT , KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_LBRC, SH_TG  , SH_TG  , KC_RBRC, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,  KC_ENT ,
+                                 ADJUST , KC_LSFT, COD_SPC, NBR_SPC , KC_ENT, KC_DEL , FUN_SPC, NBL_SPC, KC_RALT, KC_LGUI
     ),
 
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_PGUP , KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     KC_PGDN , KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
-      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
+      _______, _______, _______, _______, _______, _______,                                     KC_PGUP , KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_BSPC,
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     KC_PGDN , KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_DEL ,
+      XXXXXXX, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 
-    [_NUM] = LAYOUT(
+    [_NUMR] = LAYOUT(
       _______, _______, _______, _______, _______, _______,                                     KC_BSLS, KC_P7 , KC_P8, KC_P9, KC_EQL , KC_BSPC,
-      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     KC_AMPR, KC_P4 , KC_P5, KC_P6, KC_PLUS,  KC_DEL,
-      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______, KC_UNDS, KC_P1 , KC_P2, KC_P3, KC_SLSH,  KC_ENT,
-                                 _______, _______, _______, _______, _______, KC_NLCK, KC_MINS, KC_P0  , KC_DOT,  _______
+      _______, KC_LCTL, KC_LALT, KC_LSFT, KC_LGUI, _______,                                     KC_AMPR, KC_P4 , KC_P5, KC_P6, KC_PLUS, KC_DEL,
+      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, KC_NLCK, _______, KC_UNDS, KC_P1 , KC_P2, KC_P3, KC_SLSH, KC_ENT,
+                                 _______, _______, _______, XXXXXXX, _______, KC_SPC , KC_MINS, KC_P0  , KC_DOT,  _______
+    ),
+
+    [_NUML] = LAYOUT(
+      KC_EQL , KC_DEL , KC_P7  , KC_P8  , KC_P9  , KC_BSLS,                                     _______, _______, _______, _______, _______, _______,                                     
+      KC_PLUS, KC_BSPC, KC_P4  , KC_P5  , KC_P6  , KC_AMPR,                                     _______, KC_RGUI, KC_RSFT, KC_LALT, KC_RCTL, _______,                                     
+      KC_SLSH, KC_ENT , KC_P1  , KC_P2  , KC_P3  , KC_UNDS, _______, KC_SLCK, KC_NLCK, _______, _______, _______, _______, _______, _______, _______,  
+                                 _______, KC_DOT , KC_P0  , KC_MINS, KC_SPC , _______, XXXXXXX, _______, _______,  _______
     ),
     
     [_FUNCTION] = LAYOUT(
       _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
       _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RGUI, KC_RSFT, KC_LALT, KC_RCTL, _______,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-
-
-
-    [_SYM] = LAYOUT(
-     KC_GRV  ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_DEL ,
-     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-     KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+                                 _______, _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______
     ),
 
 
     [_CODE] = LAYOUT(
-      KC_ASTR, KC_QUES, KC_BSLS, KC_AMPR, KC_PIPE, KC_SLSH,                                     _______, KC_LCBR, KC_RCBR, _______, _______, KC_BSPC,
-      KC_PLUS, KC_EXLM, KC_MOR , KC_MINS, KC_EQL , KC_LSS ,                                     _______, KC_LPRN, KC_RPRN, _______, _______, KC_TAB ,
-      KC_HASH, KC_DOT , KC_COLN, KC_UNDS, KC_SCLN, KC_COMM, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_ENT ,
-                                 _______, _______, _______, _______, _______, _______, KC_SPC , KC_DLR , _______, _______
+      _______, KC_ASTR, KC_QUES, KC_MINS, KC_HASH, KC_TILD,                                     KC_GRV , KC_LCBR, KC_SLSH, KC_RCBR, KC_SCLN, KC_DEL ,
+      _______, KC_EXLM, KC_MOR , KC_EQL , KC_LSS , KC_PERC,                                     KC_SQOT, KC_LPRN, KC_PIPE, KC_RPRN, KC_COLN, KC_BSPC,
+      _______, KC_AT  , KC_PLUS, KC_UNDS, KC_COMM, KC_CIRC, KC_AMPR, _______, KC_NLCK, KC_UNDS, KC_QUOT, KC_LBRC, KC_BSLS, KC_RBRC, KC_COMM, KC_ENT ,
+                                 _______, _______, _______, XXXXXXX, _______, KC_SPC , KC_MINS, KC_P0  , KC_DOT,  _______
     ),
 
 
@@ -164,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC ,   KC_1 ,   KC_2 ,  KC_3  ,  KC_4  ,  KC_5 ,                                     _______, _______, _______, _______, _______, COLEMAK,
       KC_TAB , KC_LSFT,   KC_A ,  KC_W  ,  KC_D  ,  KC_Q ,                                     _______, _______, _______, _______, _______, CMK_OSX,
       KC_ENT , KC_LCTL,   KC_X ,  KC_S  ,  KC_E  ,  KC_M ,   KC_F , KC_ENT , _______, _______, _______, _______, _______, _______, _______, KC_ENT ,
-                                ADJUST  ,  NUM   , KC_SPC,   KC_E , KC_ENT , FKEYS  ,   SYM  , KC_ENT , KC_RALT, KC_LGUI
+                                ADJUST , KC_LSFT, COD_SPC, NBR_SPC , KC_ENT, KC_DEL , FUN_SPC, NBL_SPC, KC_RALT, KC_LGUI
     ),
 
 };
@@ -198,8 +201,8 @@ static void render_status(void) {
         case _NAV:
             oled_write_P(PSTR("Nav\n"), false);
             break;
-        case _SYM:
-            oled_write_P(PSTR("Symbols\n"), false);
+        case _NUMR:
+            oled_write_P(PSTR("Num Right\n"), false);
             break;
         case _FUNCTION:
             oled_write_P(PSTR("Function\n"), false);
@@ -207,14 +210,14 @@ static void render_status(void) {
         case _ADJUST:
             oled_write_P(PSTR("Adjust\n"), false);
             break;
-        case _NUM:
-            oled_write_P(PSTR("Numpad\n"), false);
+        case _NUML:
+            oled_write_P(PSTR("Num Left\n"), false);
             break;
 				case _GAMING:
             oled_write_P(PSTR("Gaming\n"), false);
             break;
         case _CODE:
-          oled_write_P(PSTR("Code Symbols\n"), false);
+          oled_write_P(PSTR("Code\n"), false);
           break;
         default:
             oled_write_P(PSTR("Undefined\n"), false);
@@ -241,7 +244,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_PGUP);
     }
   } else if (index == 1) { /* Second encoder */
-  if (layer_state_is(_NUM) || layer_state_is(_NAV)  || layer_state_is(_CODE) ) {
+  if (layer_state_is(_NUMR) || layer_state_is(_NUML) || layer_state_is(_NAV)  || layer_state_is(_CODE) ) {
        if (!clockwise) {
         tap_code(KC_UP);
       } else {
